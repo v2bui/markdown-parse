bd610ecc1cf606d8cf7c658b7350e0c571e72bcb
v2bui <88351302+v2bui@users.noreply.github.com>
2022-01-19 15:04:54 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,8 +15,12 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            if (markdown.indexOf("[", currentIndex) == -1) {
+                currentIndex = markdown.length();
+            }
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            
         }
         return toReturn;
     }
